4d0ad017128826ba6529d8896adff08dd5f7f514
jorneylopez <jorney95@live.com>
2022-08-13 11:09:56 -0600

    FE structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import PostCreate from "./PostCreate";
+
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="container">
+      <h2>Create Post</h2>
+      <PostCreate />
     </div>
   );
 }
