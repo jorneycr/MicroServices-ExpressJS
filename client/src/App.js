91fb97ae5f619fab43de10a20d355a3975c3e7ec
jorneylopez <jorney95@live.com>
2022-08-13 00:44:42 -0600

    FE readct

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import React from "react";
-
-const App = () => {
+function App() {
   return (
-    <div className="container">
-      <h1>Create Post</h1>
-      <hr />
-      <h1>Posts</h1>
+    <div className="App">
+      <h1>App</h1>
     </div>
   );
-};
+}
+
 export default App;
