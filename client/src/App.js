cf6462bd2788392c4a2c7e1b90d0a0df46c47164
jorneylopez <jorney95@live.com>
2022-08-13 00:28:27 -0600

    microservice comments

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <hr />
+      <h1>Posts</h1>
+    </div>
+  );
+};
+export default App;
