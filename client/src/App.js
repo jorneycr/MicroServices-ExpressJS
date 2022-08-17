d0f6a8cb5ffd69a1d92c422ed87bcc39745c6398
jorneylopez <jorney95@live.com>
2022-08-16 22:47:24 -0600

    added new component PostList

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 function App() {
   return (
     <div className="container">
       <h2>Create Post</h2>
       <PostCreate />
+      <hr />
+      <h1>Posts List</h1>
+      <PostList />
     </div>
   );
 }
