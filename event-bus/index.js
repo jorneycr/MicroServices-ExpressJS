6f2fec4be67b41e9eef4ac1ce8c56fe1c6aef81b
jorneylopez <jorney95@live.com>
2022-09-07 22:26:34 -0600

    adding event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  await axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  await axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  await axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
   res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log("Event-Bus service listening on 4005");
 });
--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -13,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
@@ -21,9 +22,17 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-  console.log("Listening on 4000");
+  console.log("Post Service listening on 4000");
 });
